c615ab98e602b53d531f2f141130b5ed0aa6951d
Pontus Serrander <ps223qb@student.lnu.se>
2024-09-25 01:37:21 +0200

    In weightConverter class add a method to validate the input to be a number.

--- converters/weightConverter.js
+++ converters/weightConverter.js
@@ -1,9 +1,38 @@
-class weightConverter {
-    convertKgToLbs(kg) {
-      return kg * 2.20462
-    }
+/**
+ * A class that converts weight between the metric and imperial systems.
+ */
+export class weightConverter {
 
-    convertLbsToKg(lbs) {
-      return lbs / 2.20462
+  /**
+   * Validate the input to ensure it is a number.
+   * @param {number} input - The input to validate.
+   * @throws {Error} - Throws an error if the input is not a number.
+   */
+  validateInput(input) {
+    if (typeof input !== 'number' || isNaN(input)) {
+      throw new Error('Input must be a number')
     }
+  }
+  /**
+   * Converts a weight in kilograms to pounds.
+   *
+   * @param {number} kg - The weight in kilograms to convert.  
+   * @returns {number} - The converted weight in pounds.
+   * @throws {Error} - Throws an error if the input is not a number.
+   */
+  convertKgToLbs(kg) {
+    this.validateInput(kg)
+    return kg * 2.20462
+  }
+
+  /**
+   * 
+   * @param {number} lbs - The weight in pounds to convert.
+   * @returns {number} - The converted weight in kilograms.
+   * @throws {Error} - Throws an error if the input is not a number.
+   */
+  convertLbsToKg(lbs) {
+    this.validateInput(lbs)
+    return lbs / 2.20462
+  }
 }
