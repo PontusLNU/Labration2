c9af42c88684c9f32e4c3611d3e5a3b0b3d4b4c4
Pontus Serrander <ps223qb@student.lnu.se>
2024-09-26 00:53:10 +0200

    Create tests for weightConverter class using jest.

--- converters/temperatureConverter.test.js
+++ converters/temperatureConverter.test.js
@@ -1,7 +1,8 @@
 import temperatureConverter from "./temperatureConverter.js"
+
 const tc = new temperatureConverter()
 
-describe('temperatureConverter class:', () => {
+describe('temperatureConverter class', () => {
     test('validateInput method should throw an error if the input is not a number', () => {
         expect(() => tc.validateInput('a')).toThrow('Input must be a number')
     })
---
+++ converters/weightConverter.test.js
@@ -0,0 +1,15 @@
+import weightConverter from "./weightConverter"
+
+const wc = new weightConverter()
+
+describe("weightConverter class", () => {
+    test("validateInput method should throw an error if the input is not a number", () => {
+        expect(() => wc.validateInput("b")).toThrow("Input must be a number")
+    })
+    test("convertKgToLbs method should convert 1 kg to 2.20462 lbs", () => {
+        expect(wc.convertKgToLbs(1)).toBe(2.20462)
+    })
+    test("convertLbsToKg method should convert 2.20462 lbs to 1 kg", () => {
+        expect(wc.convertLbsToKg(2.20462)).toBe(1)
+    })
+})
