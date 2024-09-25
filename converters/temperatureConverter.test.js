ab2f079faa92ad668b259f987448c8d325c79f2b
Pontus Serrander <ps223qb@student.lnu.se>
2024-09-26 00:36:00 +0200

    Create tests for temperatureConverter class using jest.

---
+++ converters/temperatureConverter.test.js
@@ -0,0 +1,14 @@
+import temperatureConverter from "./temperatureConverter.js"
+const tc = new temperatureConverter()
+
+describe('temperatureConverter class:', () => {
+    test('validateInput method should throw an error if the input is not a number', () => {
+        expect(() => tc.validateInput('a')).toThrow('Input must be a number')
+    })
+    test('convertCelsiusToFahrenheit method should convert 0 degrees celsius to 32 degrees fahrenheit', () => {
+        expect(tc.convertCelsiusToFahrenheit(0)).toBe(32)
+    })
+    test('convertFahrenheitToCelsius method should convert 32 degrees fahrenheit to 0 degrees celsius', () => {
+        expect(tc.convertFahrenheitToCelsius(32)).toBe(0)
+    })
+})
