fd3df5fd44b3755fbfd4df2949c5a5e14bd3744b
Pontus Serrander <ps223qb@student.lnu.se>
2024-09-29 23:26:20 +0200

    fix indenting in temperatureConverter test.

--- tests/temperatureConverter.test.js
+++ tests/temperatureConverter.test.js
@@ -3,13 +3,13 @@ import temperatureConverter from "../converters/temperatureConverter"
 const tc = new temperatureConverter()
 
 describe('temperatureConverter class', () => {
-    test('validateInput method should throw an error if the input is not a number', () => {
-        expect(() => tc.validateInput('a')).toThrow('Input must be a number')
-    })
-    test('convertCelsiusToFahrenheit method should convert 0 degrees celsius to 32 degrees fahrenheit', () => {
-        expect(tc.convertCelsiusToFahrenheit(0)).toBe(32)
-    })
-    test('convertFahrenheitToCelsius method should convert 32 degrees fahrenheit to 0 degrees celsius', () => {
-        expect(tc.convertFahrenheitToCelsius(32)).toBe(0)
-    })
+  test('validateInput method should throw an error if the input is not a number', () => {
+    expect(() => tc.validateInput('a')).toThrow('Input must be a number')
+  })
+  test('convertCelsiusToFahrenheit method should convert 0 degrees celsius to 32 degrees fahrenheit', () => {
+    expect(tc.convertCelsiusToFahrenheit(0)).toBe(32)
+  })
+  test('convertFahrenheitToCelsius method should convert 32 degrees fahrenheit to 0 degrees celsius', () => {
+    expect(tc.convertFahrenheitToCelsius(32)).toBe(0)
+  })
 })
