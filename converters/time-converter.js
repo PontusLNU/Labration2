21de89cb3cf7509dae9bcfc11b42518a36440c9e
Pontus Serrander <ps223qb@student.lnu.se>
2024-10-02 02:59:23 +0200

    Create a time converter and a test file to test different cases for it.

---
+++ converters/time-converter.js
@@ -0,0 +1,78 @@
+export default class TimeConverter {
+  handle24HourInput(time) {
+    let formattedTime = time.toString().replace(/[^0-9]/g, '')
+
+    if (formattedTime.length === 3) {
+      formattedTime = '0' + formattedTime
+    }
+
+    if (formattedTime.length !== 4) {
+      throw new Error('Invalid time format, please provide a time in 24-hour format')
+    }
+
+    const hours24 = formattedTime.slice(0, 2)
+    const minutes = formattedTime.slice(2, 4)
+
+    if (hours24 < 0 || hours24 > 23 || minutes < 0 || minutes > 59) {
+      throw new Error('Invalid time format, please provide a time in 24-hour format')
+    }
+
+    return { hours24, minutes }
+  }
+
+  handle12HourInput(time) {
+    let formattedTime = time.toString().replace(/[^0-9aAmpPmM]/g, '')
+
+    if (formattedTime.length === 5) {
+      formattedTime = '0' + formattedTime
+    }
+
+    if (formattedTime.length !== 6) {
+      throw new Error('Invalid time format, please provide a time in 12-hour format')
+    }
+
+    const hours12 = formattedTime.slice(0, 2)
+    const minutes = formattedTime.slice(2, 4)
+    const period = formattedTime.slice(4, 6)
+
+    if (!/^(AM|PM)$/i.test(period)) {
+      throw new Error('Invalid time format, please provide a time in 12-hour format')
+    }
+
+    if (hours12 < 0 || hours12 > 12 || minutes < 0 || minutes > 59) {
+      throw new Error('Invalid time format, please provide a time in 12-hour format')
+    }
+
+    return { hours12, minutes, period }
+  }
+
+  convert24HourTo12Hour(time) {
+    const { hours24, minutes } = this.handle24HourInput(time)
+
+    const period = hours24 >= 12 ? 'PM' : 'AM'
+    const hours12 = hours24 % 12 || 12
+
+    return `${hours12}:${minutes}${period}`
+  }
+
+  convert12HourTo24Hour(time) {
+    const { hours12, minutes, period } = this.handle12HourInput(time)
+
+    const period24 = period.toUpperCase()
+    let hours24 = parseInt(hours12)
+
+    if ( period24 === 'PM' && hours12 < 12) {
+      hours24 = hours24 + 12
+    }
+
+    if ( period24 === 'AM' && hours12 == 12) {
+      hours24 = 0
+    }
+
+    if ( period24 === 'AM' && hours24 < 10) {
+      hours24 = '0' + hours24
+    }
+
+    return `${hours24}:${minutes}`
+  }
+}
---
+++ tests/time-converter.test.js
@@ -0,0 +1,75 @@
+import TimeConverter from '../converters/time-converter.js'
+
+const tc = new TimeConverter()
+
+describe('TimeConverter class', () => {
+  test('handle24HourInput method should throw an error if the input is not in 24-hour format', () => {
+    expect(() => tc.handle24HourInput(2500)).toThrow('Invalid time format, please provide a time in 24-hour format')
+  })
+  test('handle24HourInput method should throw an error if the input is not in 24-hour format', () => {
+    expect(() => tc.handle24HourInput(23600)).toThrow('Invalid time format, please provide a time in 24-hour format')
+  })
+  test('handle24HourInput method should throw an error if the input is not in 24-hour format', () => {
+    expect(() => tc.handle24HourInput("2436")).toThrow('Invalid time format, please provide a time in 24-hour format')
+  })
+  test('convert24HourTo12Hour method should convert 09:24 to 9:24AM', () => {
+    expect(tc.convert24HourTo12Hour(924)).toBe('9:24AM')
+  })
+  test('convert24HourTo12Hour method should convert 13:37 to 1:37PM', () => {
+    expect(tc.convert24HourTo12Hour(1337)).toBe('1:37PM')
+  })
+  test('convert24HourTo12Hour method should convert 13:37 to 1:37PM', () => {
+    expect(tc.convert24HourTo12Hour("13:37")).toBe('1:37PM')
+  })
+  test('convert24HourTo12Hour method should convert 23:37 to 11:37PM', () => {
+    expect(tc.convert24HourTo12Hour("23:37")).toBe('11:37PM')
+  })
+  test('convert24HourTo12Hour method should convert 11:37 to 11:37AM', () => {
+    expect(tc.convert24HourTo12Hour("11:37")).toBe('11:37AM')
+  })
+  test('convert24HourTo12Hour method should convert 11:37 to 11:37AM', () => {
+    expect(tc.convert24HourTo12Hour(1137)).toBe('11:37AM')
+  })
+  test('convert24HourTo12Hour method should convert 11:37 to 11:37AM', () => {
+    expect(tc.convert24HourTo12Hour("1137")).toBe('11:37AM')
+  })
+  test('handle12HourInput method should throw an error if the input is not in 12-hour format', () => {
+    expect(() => tc.handle12HourInput(2300)).toThrow('Invalid time format, please provide a time in 12-hour format')
+  })
+  test('handle12HourInput method should throw an error if the input is not in 12-hour format', () => {
+    expect(() => tc.handle12HourInput('2300')).toThrow('Invalid time format, please provide a time in 12-hour format')
+  })
+  test('handle12HourInput method should throw an error if the input is not in 12-hour format', () => {
+    expect(() => tc.handle12HourInput('23:00')).toThrow('Invalid time format, please provide a time in 12-hour format')
+  })
+  test('handle12HourInput method should throw an error if the input is not in 12-hour format', () => {
+    expect(() => tc.handle12HourInput('11:00')).toThrow('Invalid time format, please provide a time in 12-hour format')
+  })
+  test('handle12HourInput method should throw an error if the input is not in 12-hour format', () => {
+    expect(() => tc.handle12HourInput('11:00km')).toThrow('Invalid time format, please provide a time in 12-hour format')
+  })
+  test('handle12HourInput method should throw an error if the input is not in 12-hour format', () => {
+    expect(() => tc.handle12HourInput('11:00ppm')).toThrow('Invalid time format, please provide a time in 12-hour format')
+  })
+  test('convert12HourTo24Hour should convert 1:37PM to 13:37', ()=> {
+    expect(tc.convert12HourTo24Hour('1:37PM')).toBe('13:37')
+  })
+  test('convert12HourTo24Hour should convert 1:37AM to 01:37', ()=> {
+    expect(tc.convert12HourTo24Hour('1:37AM')).toBe('01:37')
+  })
+  test('convert12HourTo24Hour should convert 9:33AM to 09:33', ()=> {
+    expect(tc.convert12HourTo24Hour('9:33am')).toBe('09:33')
+  })
+  test('convert12HourTo24Hour should convert 9:33pm to 21:33', ()=> {
+    expect(tc.convert12HourTo24Hour('9:33pm')).toBe('21:33')
+  })
+  test('convert12HourTo24Hour should convert 933pm to 21:33', ()=> {
+    expect(tc.convert12HourTo24Hour('933pm')).toBe('21:33')
+  })
+  test('convert12HourTo24Hour should convert 12:00AM to 00:00', ()=> {
+    expect(tc.convert12HourTo24Hour('12:00AM')).toBe('00:00')
+  })
+  test('convert12HourTo24Hour should convert 12:00PM to 12:00', ()=> {
+    expect(tc.convert12HourTo24Hour('12:00PM')).toBe('12:00')
+  })
+})
