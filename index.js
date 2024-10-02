ca435e4202e2d06002041c5b8b12a3460af92d9e
Pontus Serrander <ps223qb@student.lnu.se>
2024-10-02 22:06:56 +0200

    Import converters to index.js and then export them.

---
+++ index.js
@@ -0,0 +1,7 @@
+import TimeConverter from "./converters/time-converter"
+import VolumeConverter from "./converters/volume-converter"
+import WeightConverter from "./converters/weight-converter"
+import TemperatureConverter from "./converters/temperature-converter"
+import LengthConverter from "./converters/length-converter"
+
+export {TimeConverter, VolumeConverter, WeightConverter, TemperatureConverter, LengthConverter}
