925d4c7e7a021438f4268c849cfe3dd4eeadbd4e
Pontus Serrander <ps223qb@student.lnu.se>
2024-10-03 04:57:05 +0200

    Add JSDOC to time-converter.js

--- converters/time-converter.js
+++ converters/time-converter.js
@@ -1,4 +1,14 @@
+/**
+ * A class that converts the time between 12-hour and 24-hour formats.
+ */
 export default class TimeConverter {
+  /**
+   * Validate the input to ensure it is in the correct format.
+   *
+   * @param {string|number} time - The time to validate.
+   * @returns {object} - The time in the correct format.
+   * @throws {Error} - Throws an error if the input is not in the correct format.
+   */
   handle24HourInput(time) {
     let formattedTime = time.toString().replace(/[^0-9]/g, '')
 
@@ -20,6 +30,13 @@ export default class TimeConverter {
     return { hours24, minutes }
   }
 
+  /**
+   * Validate the input to ensure it is in the correct format.
+   *
+   * @param {string} time - The time to validate.
+   * @returns {object} - The time in the correct format.
+   * @throws {Error} - Throws an error if the input is not in the correct format.
+   */
   handle12HourInput(time) {
     let formattedTime = time.toString().replace(/[^0-9aAmpPmM]/g, '')
 
@@ -46,6 +63,13 @@ export default class TimeConverter {
     return { hours12, minutes, period }
   }
 
+  /**
+   * Converts the time from 24-hour format to 12-hour format.
+   *
+   * @param {string|number} time - The time in 24-hour format to convert.
+   * @returns {string} - The time in 12-hour format.
+   * @throws {Error} - Throws an error if the input is not in the correct format.
+   */
   convert24HourTo12Hour(time) {
     const { hours24, minutes } = this.handle24HourInput(time)
 
@@ -55,6 +79,13 @@ export default class TimeConverter {
     return `${hours12}:${minutes}${period}`
   }
 
+  /**
+   * Converts the time from 12-hour format to 24-hour format.
+   *
+   * @param {string} time - The time in 12-hour format to convert. 
+   * @returns {string} - The time in 24-hour format.
+   * @throws {Error} - Throws an error if the input is not in the correct format.
+   */
   convert12HourTo24Hour(time) {
     const { hours12, minutes, period } = this.handle12HourInput(time)
 
